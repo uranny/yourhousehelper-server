263eb7f28d3614205b909f2ff423b4e0e66f6be2
uranny <164876325+uranny@users.noreply.github.com>
2025-12-08 13:24:12 +0900

    build : reset cors config and security config

--- src/main/java/com/uranny/yourhousehelper/global/config/SecurityConfig.java
+++ src/main/java/com/uranny/yourhousehelper/global/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
