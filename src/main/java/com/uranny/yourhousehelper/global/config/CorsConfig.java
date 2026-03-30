f3aae618584aabfcf8d06a920522ee6908ef8323
uranny <164876325+uranny@users.noreply.github.com>
2026-03-30 15:53:49 +0900

    chore : change origin domain

--- src/main/java/com/uranny/yourhousehelper/global/config/CorsConfig.java
+++ src/main/java/com/uranny/yourhousehelper/global/config/CorsConfig.java
@@ -1,20 +1,25 @@
 package com.uranny.yourhousehelper.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin(allowedOrigin);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
